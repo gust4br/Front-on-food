4d94bd333ca160fe7b92bffbd3b92a23058b117a
Gustavo Gomes <gustavosgdev@outlook.com>
2022-04-11 22:50:06 -0400

    fonte e variaveis adicionadas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { Header } from '@/components';
+import { Header, Footer } from '@/components';
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Footer />
     </div>
   )
 }
