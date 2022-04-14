f3e59c04bcf64dab072cf13986555adddff5eda5
Gustavo Gomes <gustavosgdev@outlook.com>
2022-04-14 13:18:38 -0400

    paused at: 18:13

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import Styles from "./header.module.css";
 
@@ -5,7 +6,17 @@ export function Header(){
     return(
         <header>
             <nav className={Styles.nav}>
-                <div className={Styles.brand}>[logo]</div>
+                <div className={Styles.brand}>
+                    <span>
+                        
+                        <Link href="/">
+                            <a>
+                                <Image src="/images/logo.png" width={45} height={36} alt="OnFood" />
+                                <span>OnFood</span>
+                            </a>
+                        </Link>
+                    </span>
+                </div>
                 <ul>
                     <li>
                         <Link href="/">
